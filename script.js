3c4e4fd770ef7367b7d76bca55b2d7ac846d7db4
Ngawang Salaka <ngawangS1180@gmail.com>
2021-02-16 00:13:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+$(document).ready(function(){
+    $("a").hover(function(){
+    $(this).css("color", "#551a8b");
+     }, 
+     function(){
+         $(this).css("color", "#0000EE");
+        });
+});
+
